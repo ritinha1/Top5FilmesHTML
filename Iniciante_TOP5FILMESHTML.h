d8ae2a53aa4d98fba310b5a8bafa01ac90912d81
ritinha1 <136112577+ritinha1@users.noreply.github.com>
2023-06-10 15:18:15 -0300

    Create Iniciante_TOP5FILMESHTML.html

---
+++ Iniciante_TOP5FILMESHTML.h
@@ -0,0 +1,164 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TOP 15 FILME</title>
+</head>
+<style>
+    img {
+        max-width: 350px;
+        max-height: 250px;
+        width: auto;
+        height: auto;
+    }
+
+    li {
+        font-size: 25px;
+    }
+
+    p {
+        margin-right: 77%;
+        text-align: justify
+    }
+</style>
+
+<body>
+    <div>
+        <div>
+            <h1> ⭐ TOP 5 FILMES ⭐</h1>
+            <p>Conheça agora os 5 melhores filmes para assistir.</p>
+        </div>
+        <div>
+            <br>
+            <h2> ROMANCE</h2>
+            <ul>
+                <li>Táticas do Amor (2022) </li>
+                <img src="img/Taticas-do-amor-NETFLIX.webp">
+                <p>Um publicitário e uma blogueira de moda não acreditam no amor, então apostam que vão fazer o outro se
+                    apaixonar perdidamente – com táticas nada comuns. </p>
+            </ul>
+            <ul>
+                <li>Ainda Estou Aqui (2022) </li>
+                <img src="img/Ainda-Estou-Aqui-2022.jpg">
+                <p>Uma adolescente perde o amor da sua vida em um trágico acidente. Arrasada, a jovem começa a achar que
+                    ele está se comunicando com ela do além. </p>
+            </ul>
+            <ul>
+                <li>Orgulho e Preconceito (2005) </li>
+                <img src="img/orgulhoepreconceito.jpg">
+                <p>Neste conto de Jane Austen ambientado no século 19, a Sra. Bennet espera casar suas filhas com homens
+                    ricos, incluindo o recém-chegado Sr. Darcy.</p>
+            </ul>
+            <ul>
+                <li>Querido John (2010)</li>
+                <img src="img/queridojohn.jpg">
+                <p>Quando o soldado John Tyree conhece Savannah Curtis, universitária idealista, um forte romance nasce
+                    entre eles. Durante sete anos de um tumultuado relacionamento, o casal se encontra apenas
+                    esporadicamente e mantém contato por meio de cartas de amor. Porém, a correspondência entre o casal
+                    desencadeia consequências imprevisíveis.
+                </p>
+            </ul>
+            <ul>
+                <li>Por Lugares Incríveis (2020) </li>
+                <img src="img/porlugaresinscriveis.jpg">
+                <p>O enredo segue a trajetória de Violet Markey (Elle Fanning) e Theodore Finch (Justice Smith) que,
+                    unidos por uma redação da escola sobre as maravilhas do estado de Indiana, lutam para se encontrarem
+                    e encontrarem felicidade em meio aos traumas do passado.</p>
+            </ul>
+        </div>
+        <div>
+            <br>
+            <h2> COMEDIA</h2>
+            <ul>
+                <li>As Branquelas (2004) </li>
+                <img src="img/branquelas.jpg">
+                <p>As Branquelas traz uma história muito divertida de dois agentes do FBI que acabam se disfarçando de
+                    princesas ricas para se infiltrar na alta sociedade. Uma diversão total com estes dois em situações
+                    extremamente engraçadas. </p>
+            </ul>
+            <ul>
+                <li>Lua de Mel com a Minha Mãe (2022) </li>
+                <img src="img/lua-de-mel-com-a-minha-mae.jpg">
+                <p>Um filme de comédia espanhol. O longa acompanha José Luis, ele leva a mãe, Mari Carmen como
+                    acompanhante em sua viagem de lua de mel, isso depois de ser abandonado no altar, porque a viagem já
+                    estava paga. Os dois se divertem muito e o vínculo entre os dois se fortalece. O que poderia dar
+                    errado? Um filme com muitos risos e diversão. </p>
+            </ul>
+            <ul>
+                <li>A Megera Domada (2022) </li>
+                <img src="img/a-megera-domada.jpg">
+                <p>A Megera Domada é uma comédia romântica, onde uma cientista de coração partido volta para casa em
+                    busca de um recomeço. Só que o seu irmão picareta acaba de contratar um bonitão para tentar
+                    convencê-la a vender as terras da família. </p>
+            </ul>
+            <ul>
+                <li>Carnaval (2021)</li>
+                <img src="img/carnaval.jpg">
+                <p>Na comédia "Carnaval", através de um vídeo que viralizou na internet, a influenciadora Nina (Giovana
+                    Cordeiro) descobre a traição de seu namorado. Depois de acabar com o namoro, ela viaja com as amigas
+                    GKay, Sâmia Pascotto e Bruna Inocencio para o Carnaval na Bahia, onde percebe que a vida real é
+                    muito mais vibrante. </p>
+            </ul>
+            <ul>
+                <li>Minha Mãe é Uma Peça (2013) </li>
+                <img src="img/minhamaeeumapeça.jpg">
+                <p>Dona Hermínia é uma senhora de meia-idade, divorciada do marido, que a trocou por uma mulher mais
+                    jovem. Hiperativa, ela não larga o pé de seus filhos Marcelina e Juliano. Um dia, após descobrir que
+                    eles a consideram chata, ela resolve sair de casa sem avisar ninguém, deixando todos preocupados.
+                    Dona Hermínia decide visitar a querida tia Zélia para desabafar suas tristezas atuais e recordar os
+                    bons tempos do passado. </p>
+            </ul>
+        </div>
+        <div>
+            <br>
+            <h2> DRAMA</h2>
+            <ul>
+                <li>Milagre na Cela 7 (2019) </li>
+                <img src="img/milagre-na-cela-7.jpg">
+                <p>Memo, um pastor de ovelhas com deficiência mental, vive com sua filha e avó em uma vila na costa
+                    turca do mar Egeu. Um dia, sua vida é virada de cabeça para baixo quando a filha do comandante morre
+                    e Memo é acusado do assassinato e condenado à morte. </p>
+            </ul>
+            <ul>
+                <li>Olá, Adeus e Tudo Mais (2022) </li>
+                <img src="img/adeus.jpg">
+                <p>"Olá, Adeus e Tudo Mais" é uma comédia romântica encantadora, dos mesmos produtores da franquia "Para
+                    Todos os Garotos". Antes de irem para a faculdade, Clare (Talia Ryder) e Aidan (Jordan Fisher)
+                    combinam de terminar o relacionamento. Os dois relembram os momentos mais marcantes da relação, em
+                    sua última noite juntos, desde o dia em que se conheceram, o primeiro beijo e a primeira briga.
+                    Relembrando todos esses detalhes do relacionamento os dois começam a questionar se realmente é
+                    necessário colocar um ponto final. Baseada no best-seller de Jennifer E. Smith. </p>
+            </ul>
+            <ul>
+                <li>Um Ninho para Dois (2021)</li>
+                <img src="img/um-ninho-para-dois.jpg">
+                <p>"Um Ninho para Dois" acompanha o casal Lilly (Melissa McCarthy) e Jack (Chris O'Dowd) depois que o
+                    bebê deles morre. Jack acaba sendo internado em uma clínica de reabilitação devido ao luto. Lilly se
+                    dedica a construir um jardim para quando o marido voltar para casa, quando acaba sendo atormentada
+                    por um pássaro e decide procurar ajuda com um veterinário para se livrar do animal. Só que o
+                    profissional que ela encontra era um psiquiatra que decidiu se dedicar ao animais, e ela acaba
+                    recebendo alguns conselhos completamente inesperados. </p>
+            </ul>
+            <ul>
+                <li>E amanhã...o mundo todo (2020) </li>
+                <img src="img/amanha.jpg">
+                <p> Neste filme a estudante de Direito Luisa (Mala Emde) vai morar em Mannheim, Alemanha. No país, a
+                    política está mudando para ideologias cada vez mais extremistas. Assim ela se junta a um grupo
+                    anti-fascista junto com seu amigo Alfa (Noah Saavedra). O caminho fica cada vez mais perigoso e
+                    violento. </p>
+            </ul>
+            <ul>
+                <li>O Menino que Descobriu o Vento (2019)</li>
+                <img src="img/O-Menino-Que-Descobriu-O-Vento.jpg">
+                <p>Sempre esforçando-se para adquirir conhecimentos diversificados, um jovem de Malawi se cansa de
+                    assistir todos os colegas de seu vilarejo passando por dificuldades e começa a desenvolver uma
+                    inovadora turbina de vento. </p>
+            </ul>
+        </div>
+    </div>
+
+</body>
+
+</html>
